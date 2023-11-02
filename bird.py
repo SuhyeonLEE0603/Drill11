cf5a5668580f2d7ad0c9d8ca183d2010f45fff12
Lee Su Hyeon <dltngus12323@gmail.com>
2023-11-02 10:58:40 +0900

    Bird 클래스 추가

---
+++ bird.py
@@ -0,0 +1,83 @@
+# 이것은 각 상태들을 객체로 구현한 것임.
+import random
+
+from pico2d import get_time, load_image, load_font, clamp, SDL_KEYDOWN, SDL_KEYUP, SDLK_SPACE, SDLK_LEFT, SDLK_RIGHT
+from ball import Ball, BigBall
+import game_world
+import game_framework
+
+# Bird Run Speed
+# fill here
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+# Bird Action Speed
+# fill here
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
+class Run:
+
+    @staticmethod
+    def enter(boy, e):
+        pass
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+        boy.x += boy.dir * RUN_SPEED_PPS * game_framework.frame_time
+        boy.x = clamp(25, boy.x, 1600 - 25)
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(int(boy.frame) * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+
+class StateMachine:
+    def __init__(self, bird):
+        self.bird = bird
+        self.cur_state = Run
+
+    def start(self):
+        self.cur_state.enter(self.bird, ('NONE', 0))
+
+    def update(self):
+        self.cur_state.do(self.bird)
+
+    def handle_event(self, e):
+        pass
+
+    def draw(self):
+        self.cur_state.draw(self.bird)
+
+
+class Bird:
+    def __init__(self):
+        self.x, self.y = random.randint(50, 800), 400
+        self.frame = 0
+        self.action = 3
+        self.face_dir = 1
+        self.dir = 0
+        self.image = load_image('bird_animation.png')
+        self.state_machine = StateMachine(self)
+        self.state_machine.start()
+        self.font = load_font('ENCR10B.TTF', 16)
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        pass
+
+    def draw(self):
+        self.state_machine.draw()
+        self.font.draw(self.x - 60, self.y + 50, f'(Time: {get_time():.2f})', (255, 255, 0))
